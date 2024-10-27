fb685afa282397d7d28aa8a4c4ca896a91401351
Brecht Kuppens <brecht.kuppens@gmail.com>
2024-10-27 07:37:22 +0100

    two tanks, zooming camera, moving turret

--- tank.gd
+++ tank.gd
@@ -1,11 +1,29 @@
 extends RigidBody2D
 
+@export var drive_left_action := "ui_left"
+@export var drive_right_action := "ui_left"
+@export var turret_up_action := "ui_up"
+@export var turret_down_action := "ui_down"
+@export var fire_action := "ui_accept"
+
+@export var turret_min_angle: float = -60.0
+@export var turrent_max_angle: float = 0.0
+
 const FULL_MOTOR_POWER: float = 40_000
 const MIN_POWER: float = 4_000
 const MAX_VELOCITY: float = 300
 
+const TURRET_ROTATE_SPEED: float = 50.0
+
+@onready var turret: Sprite2D = $Turret
+
+func _physics_process(delta: float) -> void:
+	var turret_movement = Input.get_axis(turret_up_action, turret_down_action) * delta * TURRET_ROTATE_SPEED
+	turret.rotation_degrees = clamp(turret.rotation_degrees + turret_movement, turret_min_angle, turrent_max_angle)
+
+
 func _integrate_forces(state: PhysicsDirectBodyState2D):
-	var tank_movement: float = Input.get_axis("ui_left", 'ui_right')
+	var tank_movement: float = Input.get_axis(drive_left_action, drive_right_action)
 	if tank_movement == 0:
 		self.physics_material_override.friction = 1.0
 	else:
--- world.gd
+++ world.gd
@@ -2,7 +2,12 @@ extends Node2D
 
 @onready var camera: Camera2D = $Camera2D
 @onready var tank_1: RigidBody2D = $Tank1
+@onready var tank_2: RigidBody2D = $Tank2
 
+const BORDER_SIZE := 400
 
 func _process(_delta: float) -> void:
-	camera.position = tank_1.position
+	camera.position = (tank_1.position + tank_2.position) * 0.5
+	var zoom = get_viewport().size.x / abs(tank_1.position.x - tank_2.position.x - 2 * BORDER_SIZE)
+	zoom = min(zoom, 0.75)
+	camera.zoom = Vector2(zoom, zoom)
