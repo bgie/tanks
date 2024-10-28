d787984271804ae06ee0f9081e24ca5edc81e0d9
Brecht Kuppens <brecht.kuppens@gmail.com>
2024-10-28 21:07:13 +0100

    fixed bullet orientation

--- bullet.gd
+++ bullet.gd
@@ -1,16 +1,17 @@
 extends RigidBody2D
 
-const WIND_RESISTANCE_FACTOR: float = 0.005
+const WIND_RESISTANCE_FACTOR: float = 0.5
 
 func _integrate_forces(state: PhysicsDirectBodyState2D):
 	var speed = self.linear_velocity.length()
 	if speed > 0.1:
 		var wind_resistance_angle = state.linear_velocity.angle() - rotation
-		if wind_resistance_angle > PI:
+		while wind_resistance_angle > PI:
 			wind_resistance_angle -= 2*PI
-		elif wind_resistance_angle < PI:
+		while wind_resistance_angle < -PI:
 			wind_resistance_angle += 2*PI
-		state.apply_torque(wind_resistance_angle * wind_resistance_angle * speed * WIND_RESISTANCE_FACTOR * mass)
+		var torque: float = wind_resistance_angle * speed * WIND_RESISTANCE_FACTOR * mass
+		state.apply_torque(torque)
 
 func _on_warhead_area_body_entered(body: Node2D) -> void:
 	if body != self:
